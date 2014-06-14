9ef34aa774dedf1f6ea1e8a822ec4879f4fcb59c
highpower <xuhaohp@gmail.com>
2014-06-13 22:56:34 -0400

    Update Docs

--- doc/Site/Scripts/trans.js
+++ doc/Site/Scripts/trans.js
@@ -3,7 +3,7 @@
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('external_software_files/external_softwareMoz.css')
+{loadMozillaCSS('css/Moz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
